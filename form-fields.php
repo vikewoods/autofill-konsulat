e63166be7d05cc0f20dd101cc3ce4f9ac697e499
Vik Ewoods <vik.ewoods@gmail.com>
2015-03-25 17:39:41 +0200

    Added some Vinica defines

--- form-fields.php
+++ form-fields.php
@@ -1,7 +1,33 @@
 <?php
 
+// Config fields version number
+// @VERSION - Application version (File version state)
+define('VERSION', '1.0.1');
 
+// Language field
+define('LANGUAGE_F', 'ctl00$ddlWersjeJezykowe');
 
+// First page fields
+define('COUNTRY_REGION_F', 'ctl00$tresc$cbListaKrajow');
+define('DIPLOMATIC_MISSION_F', 'ctl00$tresc$cbListaPlacowek');
 
+// Urls data e-konsulat
+define('VISA_VINICA_URL', 'IDPlacowki=96');
+
+// Vinica registration form
+// https://secure.e-konsulat.gov.pl/Uslugi/RejestracjaTerminu.aspx?IDUSLUGI=1&IDPlacowki=96
+define('VINICA_NATIONAL_VISA_REG', 'IDUSLUGI=1');
+
+// Vinica fill personal data form
+// https://secure.e-konsulat.gov.pl/Wiza/FormularzWiza.aspx?guid=0f4ae99553d141769f9614d08ffa0296
+define('VINICA_FILL_FORM', 'guid=');
+
+// Vinica schengen visa registration form
+// https://secure.e-konsulat.gov.pl/Uslugi/RejestracjaTerminu.aspx?IDUSLUGI=8&IDPlacowki=96
+define('VINICA_SCHENGEN_VISA_REG', 'IDUSLUGI=8');
+
+// Vinica schengen fill personal data form
+// https://secure.e-konsulat.gov.pl/Wiza/FormularzWiza.aspx?guid=0f4ae99553d141769f9614d08ffa0296
+define('VINICA_FILL_SCHENGEN_FORM', 'guid=');
 
 ?>
